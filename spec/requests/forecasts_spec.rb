e723ff02bb25bd9883e43c8e485c26cc11e197ad
James Jeffers <jdjeffers@protonmail.com>
2024-01-28 11:59:06 -0500

    More lint updates.

--- spec/requests/forecasts_spec.rb
+++ spec/requests/forecasts_spec.rb
@@ -3,6 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Forecasts', type: :request do
+  let(:open_meteo_response) do
+    {
+      "product": 'civillight',
+      "init": '2021-03-16T00:00:00Z',
+      "dataseries": [
+        {
+          "timepoint": 3,
+          "cloudcover": 0,
+          "lifted_index": 0,
+          "prec_type": 'none',
+          "prec_amount": 0,
+          "temp2m": 9,
+          "rh2m": 71,
+          "wind10m": {
+            "direction": 0,
+            "speed": 0
+          },
+          "weather": 'clearsky'
+        }
+      ]
+    }
+  end
+
   describe 'GET /show' do
     it 'returns http success without query' do
       get forecast_path
@@ -14,7 +37,9 @@ RSpec.describe 'Forecasts', type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it 'returns http success with a query but no geo-located data' do
+    it 'returns http success with a query ' do
+      allow_any_instance_of(ForecastsHelper).to receive(:fetch_weather).and_return([open_meteo_response])
+
       get forecast_path, params: { query: '123 Oak Street' }
       expect(response).to have_http_status(:success)
     end
